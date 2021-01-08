132aa88cdc6f34ac09d2a30e80be6f0d163af172
p-cruz <paulacruz@iesgregorioprieto.com>
2021-01-08 19:55:09 +0100

    Quito el css

--- index.js
+++ index.js
@@ -5,62 +5,63 @@ async function start() {
   try {
     const response = await fetch("https://dog.ceo/api/breeds/list/all")
     const data = await response.json()
-    createBreedList(data.message)
+    creaListaRazas(data.message)
   } catch (e) {
-    console.log("There was a problem fetching the breed list.")
+    console.log("Hubo un problema cargando la lista de razas de perro.")
   }
 }
 
 start()
 
-function createBreedList(breedList) {
-  document.getElementById("breed").innerHTML = `
-  <select onchange="loadByBreed(this.value)">
-        <option>Choose a dog breed</option>
-        ${Object.keys(breedList).map(function (breed) {
-          return `<option>${breed}</option>`
+function creaListaRazas(listaRazas) {
+  document.getElementById("raza").innerHTML = `
+  <select onchange="cargaPorRaza(this.value)">
+        <option>Elige una raza...</option>
+        ${Object.keys(listaRazas).map(function (raza) {
+          return `<option>${raza}</option>`
         }).join('')}
       </select>
   `
 }
 
-async function loadByBreed(breed) {
-  if (breed != "Choose a dog breed") {
-    const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`)
+async function cargaPorRaza(raza) {
+  if (raza != "Elige una raza...") {
+    const response = await fetch(`https://dog.ceo/api/breed/${raza}/images`)
     const data = await response.json()
     createSlideshow(data.message)
   }
 }
 
 function createSlideshow(images) {
-  let currentPosition = 0
-  clearInterval(timer)
-  clearTimeout(deleteFirstPhotoDelay)
-  
+  console.log(images)
+  document.getElementById("slideshow").innerHTML=""
+  contador=1;
   if (images.length > 1) {
-    document.getElementById("slideshow").innerHTML = `
-  <div class="slide" style="background-image: url('${images[0]}')"></div>
-  <div class="slide" style="background-image: url('${images[1]}')"></div>
-  `
-  currentPosition += 2
-  if (images.length == 2) currentPosition = 0
-  timer = setInterval(nextSlide, 3000)
-  } else {
-    document.getElementById("slideshow").innerHTML = `
-  <div class="slide" style="background-image: url('${images[0]}')"></div>
-  <div class="slide"></div>
-  `
-  }
-
-  function nextSlide() {
-    document.getElementById("slideshow").insertAdjacentHTML("beforeend", `<div class="slide" style="background-image: url('${images[currentPosition]}')"></div>`)
-    deleteFirstPhotoDelay = setTimeout(function () {
-      document.querySelector(".slide").remove()
-    }, 1000)
-    if (currentPosition + 1 >= images.length) {
-      currentPosition = 0
-    } else {
-      currentPosition++
-    }
+    cadena = `<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">`
+    images.forEach(function(item,index){
+      if (contador==1){
+        cadena +=`<div class="carousel-item active">
+                    <img src="${item}" class="d-block w-100" alt="imagen">
+                </div>`
+        contador++;
+      }else{
+          cadena +=`<div class="carousel-item">
+                        <img src="${item}" class="d-block w-100" alt="imagen">
+                    </div>`
+      }
+    });
+    cadena+=`</div>
+              <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+              </a>
+              <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+              </a>
+            </div>
+            `
+    document.getElementById("slideshow").insertAdjacentHTML("beforeend", cadena)
   }
 }
